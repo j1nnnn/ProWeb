bb505041776618f362d74d6011b3b4e9d36a00ed
Jinwoo Lim <jl966@njit.edu>
2021-05-15 16:37:36 -0400

    commit on main

---
+++ Untitled-1.php
@@ -0,0 +1,39 @@
+<!-- from the tutorial -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to my professional website!</title>
+    <!-- from the tutorial // this is where style.css is being linked -->
+    <link rel="stylesheet" type="text/css" href="./style.css"> 
+</head>
+<body>  
+    <div class="icon-bar col-12">
+        <a class="active" href="#">Bio</a>
+        <a class="active" href="#">Education</a>
+        <a class="active" href="#">Organizations</a>
+        <a class="active" href="#">Experience</a>
+        <a class="active" href="#">Projects</a>
+    </div>
+    <div class="main-container">
+        <div class="header">
+            <h1>Jinwoo's Website</h1>
+        </div>
+        <div class="main-content">
+            <img src="cspic.jpg">
+                 <p>Hello! Welcome to my Professional Portfolio! Here, 
+            you will explore my amazing set of skills, capabilities, and 
+            potential! Explore for more! </p>
+        </div>
+    </div>
+</body>
+</html>
+
+<?php
+//This is if we need SESSION array
+session_start()
+
+
+
+
+
+?>
